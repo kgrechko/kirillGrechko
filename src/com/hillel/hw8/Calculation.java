59dbdfa82a55f7b5835af287abbb52bc11ed3478
kgrechko <86965025+kgrechko@users.noreply.github.com>
2021-12-13 17:55:17 +0300

    Added Classes ArythmeticCalcularot, inherited EngineeringCalculator class from ArythmeticCalculator for hw8

---
+++ src/com/hillel/hw8/Calculation.java
@@ -0,0 +1,7 @@
+package com.hillel.hw8;
+
+public class Calculate {
+    public static void main(String[] args) {
+        ArythmeticCalculator.Calculate (ArythmeticCalculator.firstDouble(), ArythmeticCalculator.secondDouble());
+    }
+}
---
+++ src/com/hillel/hw8/EngineeringCalculator.java
@@ -0,0 +1,2 @@
+package com.hillel.hw8;public class EngineeringCalculator {
+}
