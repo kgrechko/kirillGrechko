e54ac71e1234cd460e1b8ad988797ae1c0b78db9
kgrechko <86965025+kgrechko@users.noreply.github.com>
2022-01-16 20:15:23 +0300

    Added exception to te cards game

---
+++ src/com/hillel/hw10/NewDeckException.java
@@ -0,0 +1,9 @@
+package com.hillel.hw10;
+
+public class NewDeckException extends RuntimeException {
+    NewDeckException(String message) {
+        super(message);
+
+    }
+
+}
---
+++ src/com/hillel/hw10/PokerMachine.java
@@ -0,0 +1,83 @@
+package com.hillel.hw10;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class PokerMachine {
+
+
+    public Card[] createDeck()
+    {
+        String[] suites = {"clubs", "spades", "diamonds", "hearts"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        Card[] deck = new Card[suites.length*values.length];
+
+        int count = 0;
+        for (String suite : suites)
+        {
+            for (String value : values)
+            {
+                deck[count ++] = new Card(suite, value);
+            }
+        }
+        return deck;
+    }
+
+    public void shuffleCards(Card[] deck)
+    {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            int in = random.nextInt(52);
+            int out = random.nextInt(52);
+
+            Card temp = deck[in];
+            deck[in] = deck[out];
+            deck[out] = temp;
+        }
+    }
+
+    public void serveCards(Card[] deck)
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println();
+
+            for (int j = 0; j < 5; j++)
+            {
+                System.out.println("player " + i + " receives " + deck[5 * i + j]);
+                if (i % 5 == 5 - 1) ;
+
+            }
+        }
+    }
+
+    public boolean shuffleOption() throws NewDeckException
+    {
+        boolean shuffled = false;
+        System.out.println("Do you want to shuffle cards? (yes/no)");
+        Scanner scanner = new Scanner(System.in);
+        String i = scanner.nextLine();
+
+        switch (i) {
+            case "yes":
+                shuffled = true;
+                break;
+
+            case "no":
+                shuffled = false;
+                break;
+        }
+
+        if (shuffled == false) {
+            throw new NewDeckException("deck is not shuffled");
+        }
+
+        return shuffled;
+    }
+
+}
+
+
+
