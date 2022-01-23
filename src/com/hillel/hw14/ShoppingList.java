12f28998706d403b56c8660cfdb84638155716c2
kgrechko <86965025+kgrechko@users.noreply.github.com>
2022-01-23 20:17:08 +0300

    Added coide for Collections home work

---
+++ src/com/hillel/hw14/ShoppingList.java
@@ -0,0 +1,44 @@
+package com.hillel.hw14;
+
+import javax.sound.midi.Soundbank;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        ArrayList<String> ProductList = new ArrayList<String>();
+        boolean Full = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter products and quantity:");
+
+        do
+        {
+            System.out.println("Enter product name (or stop to end adding products): ");
+            String Product = scanner.nextLine();
+
+                if (Product.equals("stop"))
+                {
+                    Full = false;
+                }
+
+                else
+                {
+                    System.out.println("quantity: ");
+                    int quantity = Integer.parseInt(scanner.nextLine());
+                    addProduct(ProductList, Product, quantity);
+                }
+
+        } while (Full == true);
+
+        System.out.println(ProductList);
+        System.out.println("Amount of products is " + ProductList.size());
+    }
+
+    public static void addProduct (ArrayList list, String product, int quantity)
+    {
+        for (int i = 0; i < quantity; i++)
+        {list.add(product);}
+    }
+}
+
